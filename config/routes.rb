f35786ec49ab4919ad4fd9574007122f2b6f2d9e
Nicolas Feer <atmosfeer@gmail.com>
2019-08-28 13:55:01 +0200

    Compare logic working, one user in seed for testing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#survey'
   get 'politicians', to: 'pages#politicians'
+  post '/', to: 'pages#results', as: :result
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 require 'csv'
 require 'pry-byebug'
 
-Politician.destroy_all
+Answer.destroy_all
 PolAnswer.destroy_all
+Politician.destroy_all
+DummyUser.destroy_all
 
 WEIGHTS = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'PASS' => 0 }
 
@@ -18,6 +20,18 @@ CSV.foreach(csv_path, csv_options) do |row|
   attributes[:image] = clean_string("#{row[0].split.first.downcase}_#{row[1].downcase}.jpg")
   pol = Politician.create!(attributes)
   (1..27).each do |qnum|
-    p_ans = PolAnswer.create!(question_number: qnum, politician: pol, weight: WEIGHTS[row["answer#{qnum}"]] )
+    begin
+      p_ans = PolAnswer.create!(question_number: qnum, politician: pol, weight: WEIGHTS[row["answer#{qnum}"]] )
+    rescue ActiveRecord::RecordInvalid
+      binding.pry
+      puts "h"
+    end
   end
+end
+
+minna = DummyUser.create
+answers = %w(B B C B C C B B D D B C B C C D D C D D D D C B D C D)
+
+answers.each_with_index do |answer, idx|
+  ans = Answer.create!(question_number: idx + 1, dummy_user: minna, weight: WEIGHTS[answer])
 end
