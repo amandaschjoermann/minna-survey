26464ec59406c576d92d9182a48a1991ead2bff9
Nicolas Feer <atmosfeer@gmail.com>
2019-08-28 15:02:46 +0200

    implemented result logic

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get 'politicians', to: 'pages#politicians'
 
 
-  get 'result', to: 'pages#result'
 
-  post '/', to: 'pages#results', as: :result
+  get 'result/:user_id', to: 'pages#result', as: :top_five
+
+  post '/', to: 'pages#results', as: :result_form
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
