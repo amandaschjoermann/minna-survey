8e37585c659f436243b8af3eaa1881272bd7687d
Amanda Schjørmann <amandaschjoermann@gmail.com>
2019-08-28 15:05:00 +0200

    add a result card

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'result', to: 'pages#result'
 
-  post '/', to: 'pages#results', as: :result
+  post '/', to: 'pages#results', as: :results
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
